ed2e1607c233879acfab54866daa20c8204afbde
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-11-01 04:12:01 +0100

    Add script for uploading to IPFS

--- 2-create-package.js
+++ 2-create-package.js
@@ -2,14 +2,23 @@ const fs = require("fs");
 
 const { EthPM } = require("ethpm");
 
-
+/*
+ * Create a package by uploading contract source file and manifest to IPFS
+ */
 async function main() {
   /*
    * initialize EthPM
    */
   const ethpm = await EthPM.configure({
     manifests: "ethpm/manifests/v2",
-  }).connect();
+    storage: "ethpm/storage/ipfs",
+  }).connect({
+    ipfs: {
+      host: "ipfs.infura.io",
+      port: "5001",
+      protocol: "https"
+    }
+  });
   console.log("EthPM configured.");
   console.log();
 
@@ -19,6 +28,13 @@ async function main() {
   const contractPath = "./contracts/Owned.sol";
   const contract = fs.readFileSync(contractPath).toString();  // fromBuffer
   console.log("Contract source read from disk.");
+  console.log();
+
+  /*
+   * write to IPFS
+   */
+  const contractUri = await ethpm.storage.write(contract);
+  console.log("Contract source uploaded to IPFS.");
   console.log();
 
   /*
@@ -38,8 +54,8 @@ async function main() {
       }]
     },
     sources: {
-      /* Embed source code inside manifest for now */
-      [contractPath]: contract
+      /* use the IPFS URI for the contract file */
+      [contractPath]: contractUri
     },
     contractTypes: {},
     deployments: new Map(),
@@ -55,10 +71,18 @@ async function main() {
   console.log("Manifest generated.");
   console.log();
 
-  console.log("Full manifest:");
-  console.log("==============");
+  /*
+   * submit manifest to IPFS
+   */
+  const manifestUri = await ethpm.storage.write(manifest);
+  console.log("Manifest uploaded to IPFS.");
   console.log();
-  console.log(manifest);
+
+  console.log("Manifest URI:");
+  console.log("=============");
+  console.log();
+
+  console.log(manifestUri.href);
 }
 
 main();
